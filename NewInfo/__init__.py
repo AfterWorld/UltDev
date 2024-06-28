1f1c88ea39428446c8933cdfa68c1b8158a93508
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2024-06-28 16:51:43 -0400

    Update __init__.py

--- NewInfo/__init__.py
+++ NewInfo/__init__.py
@@ -1,4 +1,5 @@
 from .onepiece_info import OnePieceInfo
 
-async def setup(bot):
-    bot.add_cog(OnePieceInfo(bot))
+async def setup(bot: Red):
+    cog = OnePieceInfo(bot)
+    await bot.add_cog(cog)
