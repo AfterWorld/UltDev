bf8e906e2bfc477fdd04ee97666c082088858e17
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2024-06-28 16:36:08 -0400

    Update and rename onepieceserver.py to modified_core.py

---
+++ OnePieceServer/modified_core.py
@@ -0,0 +1,32 @@
+import discord
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.commands import Core
+import sys
+
+class ModifiedCore(Core):
+    def __init__(self, bot: Red):
+        super().__init__(bot)
+
+    @commands.command()
+    async def info(self, ctx):
+        """Shows information about the Grand Line Discord <:strawhat:1243924879045034075>."""
+        python_version = "{}.{}.{}".format(*sys.version_info[:3])
+        dpy_version = discord.__version__
+        red_version = self.bot.__version__
+        ping = round(self.bot.latency * 1000)
+
+        title = "Grand Line Discord <:strawhat:1243924879045034075>"
+        embed = discord.Embed(title=title, description="Ahoy, pirates! Welcome to our One Piece themed Discord server. I'm Sunny, the bot sailing these digital seas. I'm always on deck and ready to help whenever a nakama needs me. Now, let me tell you about my friend [Red](https://github.com/Cog-Creators/Red-DiscordBot/tree/V3/develop/redbot), the system that powers me.")
+        embed.set_thumbnail(url="https://example.com/sunny_bot_avatar.png")
+        embed.add_field(inline=False, name=' ', value='I (Sunny <:strawhat:1243924879045034075>) am an instance of Red-DiscordBot. If you want a bot like me (because I\'m as SUPER as Franky!), you can create your own by following the [Red installation docs](https://docs.discord.red/en/stable/install_guides/index.html).')
+        embed.add_field(inline=False, name=' ', value='Use `!credits` and `!findcog` to view the other sources used in Sunny.')
+        embed.add_field(inline=False, name=' ', value="You might be wondering how to get Sunny for your own server. Currently, Sunny is a private bot for this Grand Line, but if you want to set sail with a bot like Sunny, you'll need to contact our Shipwright (server admin). Or better yet, build your own Red instance and customize it to be as SUPER as you want!")
+        embed.add_field(inline=False, name="", value=(f"**<:log_pose:1252942734738591776> Python Version: {python_version} \n<:den_den_mushi:1252942959855276143> discord.py: {dpy_version} \n<:sunny:1244503516039348234> Red version: {red_version} \n🏴‍☠️ Ping : {ping}ms\n**"))
+        await ctx.send(embed=embed)
+
+async def setup(bot: Red):
+    core = bot.get_cog("Core")
+    if core:
+        await bot.remove_cog("Core")
+    await bot.add_cog(ModifiedCore(bot))
