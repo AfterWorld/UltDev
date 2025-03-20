d980a46f3cc5c099c8a4512fd0c8f27c2f24493f
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-03-20 11:19:33 -0400

    Update __init__.py

--- skadoosh/__init__.py
+++ skadoosh/__init__.py
@@ -1,3 +1,8 @@
-from .skadosh import setup
+from redbot.core.bot import Red
+from .skadosh import Prune
 
 __red_end_user_data_statement__ = "This cog temporarily stores message deletion logs from users. The logs are uploaded to mclo.gs and only URLs are stored in memory."
+
+async def setup(bot: Red):
+    cog = Prune(bot)
+    await bot.add_cog(cog)
