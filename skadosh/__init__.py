6438ce8ca85ae0e3b29530768c5cf8121307769a
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-03-20 11:28:03 -0400

    Delete skadosh directory

--- skadosh/__init__.py
+++
@@ -1,8 +0,0 @@
-from redbot.core.bot import Red
-from .skadosh import Skadosh
-
-__red_end_user_data_statement__ = "This cog temporarily stores message deletion logs from users. The logs are uploaded to mclo.gs and only URLs are stored in memory."
-
-async def setup(bot: Red):
-    cog = Skadosh(bot)
-    await bot.add_cog(cog)
