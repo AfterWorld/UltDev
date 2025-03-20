881331004980171621678acc53d33eef2ba120da
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-03-20 11:18:36 -0400

    Create __init__.py

---
+++ skadoosh/__init__.py
@@ -0,0 +1,3 @@
+from .skadosh import setup
+
+__red_end_user_data_statement__ = "This cog temporarily stores message deletion logs from users. The logs are uploaded to mclo.gs and only URLs are stored in memory."
