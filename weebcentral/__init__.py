ae587b7485dbd534c5c1e4316f7674d0893b32b2
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-03-22 14:36:29 -0400

    Update __init__.py

--- weebcentral/__init__.py
+++ weebcentral/__init__.py
@@ -1,6 +1,6 @@
 from redbot.core.bot import Red
 
-from .weeb import WeebCentral
+from .weeb import MangaDexTracker
 
 async def setup(bot: Red):
-    await bot.add_cog(WeebCentral(bot))
+    await bot.add_cog(MangaDexTracker(bot))
